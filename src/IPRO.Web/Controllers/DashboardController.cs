bc6f49ffea19ad50903b4fff33588e1f4ce846ab
Bahman Motamed <bmotamed@iproadvisers.com>
2026-07-07 21:16:13 -0400

    Add dashboard follow-up reminders

--- src/IPRO.Web/Controllers/DashboardController.cs
+++ src/IPRO.Web/Controllers/DashboardController.cs
@@ -1,8 +1,10 @@
 using System.Security.Claims;
 using IPRO.Billing;
 using IPRO.Business.Interfaces;
+using IPRO.DataAccess;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace IPRO.Web.Controllers;
 
@@ -13,10 +15,11 @@ public class DashboardController : Controller
     private readonly INewsLetterService _newsletters;
     private readonly IWebsiteService _websites;
     private readonly IBillingService _billing;
+    private readonly IPRODbContext _db;
     private int AgentId => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
 
-    public DashboardController(IClientService clients, INewsLetterService newsletters, IWebsiteService websites, IBillingService billing)
-    { _clients = clients; _newsletters = newsletters; _websites = websites; _billing = billing; }
+    public DashboardController(IClientService clients, INewsLetterService newsletters, IWebsiteService websites, IBillingService billing, IPRODbContext db)
+    { _clients = clients; _newsletters = newsletters; _websites = websites; _billing = billing; _db = db; }
 
     public async Task<IActionResult> Index()
     {
@@ -26,6 +29,19 @@ public class DashboardController : Controller
         ViewBag.Website         = await _websites.GetByAgentIdAsync(agentId);
         ViewBag.Subscription    = await _billing.GetActiveSubscriptionAsync(agentId);
         ViewBag.AgentName       = User.FindFirstValue("FullName");
+        ViewBag.OverdueFollowUpCount = await _db.ClientFollowUps
+            .CountAsync(f => f.Client.AgentUserId == agentId && !f.IsCompleted && f.DueAt.Date < DateTime.Today);
+        ViewBag.TodayFollowUpCount = await _db.ClientFollowUps
+            .CountAsync(f => f.Client.AgentUserId == agentId && !f.IsCompleted && f.DueAt.Date == DateTime.Today);
+        ViewBag.UpcomingFollowUpCount = await _db.ClientFollowUps
+            .CountAsync(f => f.Client.AgentUserId == agentId && !f.IsCompleted && f.DueAt.Date > DateTime.Today);
+        ViewBag.FollowUps = await _db.ClientFollowUps
+            .Include(f => f.Client)
+            .Where(f => f.Client.AgentUserId == agentId && !f.IsCompleted)
+            .OrderBy(f => f.DueAt)
+            .ThenBy(f => f.Client.LastName)
+            .Take(8)
+            .ToListAsync();
         return View();
     }
 }
