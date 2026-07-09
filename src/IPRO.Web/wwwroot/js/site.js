cf888f23eabbef803c69e67f30bfdc1c95d6628b
Bahman Motamed <bmotamed@iproadvisers.com>
2026-07-09 13:58:10 -0400

    Replace newsletter rich text editor

--- src/IPRO.Web/wwwroot/js/site.js
+++ src/IPRO.Web/wwwroot/js/site.js
@@ -1,15 +1,4 @@
-// Initialize TinyMCE on all rich text areas
 document.addEventListener('DOMContentLoaded', function () {
-    if (typeof tinymce !== 'undefined') {
-        tinymce.init({
-            selector: '.rich-text-editor:not([data-custom-rich-editor="true"])',
-            plugins: 'lists link image table code',
-            toolbar: 'undo redo | bold italic underline | alignleft aligncenter alignright | bullist numlist | link image | code',
-            height: 350,
-            menubar: false
-        });
-    }
-
     // Auto-dismiss alerts after 5 seconds
     setTimeout(function () {
         document.querySelectorAll('.alert-dismissible').forEach(function (el) {
